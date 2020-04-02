ac9b0fb74ba6882c81324825e9c140699403100a
Paul <paul.vanbreda@student.rocvf.nl>
2020-04-02 13:15:16 +0200

    h3 opdracht 2 af

---
+++ h3/Opdracht2/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP opdrachten</title>
+<body>
+<?php
+for($i = 0; $i<=90; $i++){
+    for ($j = 0; $j<$i; $j++){
+        echo "*";
+    }
+    echo "*<br>";
+}
+?>
+</body>
+</head>
+</html>
+
+
