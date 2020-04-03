985315ed08f4f9c4cdb8e3b907f423271583db7b
Paul <paul.vanbreda@student.rocvf.nl>
2020-04-03 10:56:59 +0200

    h3 opdracht 4 af

---
+++ h3/Opdracht4/index.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP opdrachten</title>
+    <style>
+        body {
+            text-align: center;
+        }
+
+        .rood {
+            border: red solid 5px;
+        }
+
+        .groen {
+            border: green solid 5px;
+        }
+    </style>
+<body>
+<?php
+for ($i = 1; $i <= 9; $i++) {
+    if ($i % 2 == 0) {
+        $class = "class='rood'";
+    } else {
+        $class = "class='groen'";
+    }
+    echo "img" . $class . " src='img/apen/aap" . $i . ".jpg'>";
+}
+?>
+</body>
+</head>
+</html>
+
+
