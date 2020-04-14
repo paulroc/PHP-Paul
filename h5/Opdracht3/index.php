5531b9ed0e56e0a06b28826ac07758e03222c9e5
Paul <paul.vanbreda@student.rocvf.nl>
2020-04-14 11:34:22 +0200

    h5 opdracht3 af
    Signed-off-by: Paul <paul.vanbreda@student.rocvf.nl>

--- h5/Opdracht3/index.php
+++ h5/Opdracht3/index.php
@@ -1,1 +1,21 @@
 <?php
+print_r($_POST);
+
+if ($_POST['voornaam']==""){
+    echo "Je moet nog een naam invullen!";
+    echo "<br><a href=\"index.html\">Terug naar het Formulier</a>a>";
+};
+
+if ($_POST['adres']==""){
+    echo "Je moet nog een adres invullen!";
+    echo "<br><a href=\"index.html\">Terug naar het Formulier</a>a>";
+};
+if ($_POST['email']==""){
+    echo "Je moet nog een email invullen!";
+    echo "<br><a href=\"index.html\">Terug naar het Formulier</a>a>";
+};
+
+if ($_POST['wachtwoord']==""){
+    echo "Je moet nog een wachtwoord invullen!";
+    echo "<br><a href=\"index.html\">Terug naar het Formulier</a>a>";
+};
