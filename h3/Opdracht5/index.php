fb626dfbea88572997bf77c93770948c2a246e2f
Paul <paul.vanbreda@student.rocvf.nl>
2020-06-09 11:13:37 +0200

    h7 af
    Signed-off-by: Paul <paul.vanbreda@student.rocvf.nl>

--- h3/Opdracht5/index.php
+++ h3/Opdracht5/index.php
@@ -11,17 +11,35 @@
 </head>
 <body>
 <?php
-$leeftijd=3;
-$bedrag=10;
-if ($leeftijd>65){
-    $bedrag=$bedrag*0.5;
+define("FARE", 10);
+define("SENIOR_AGE", 65);
+define("SENIOR_DISCOUNT", 50);
+define("JUIOR_AGE", 12);
+define("JUNIOR_DISCOUNT", 50);
+define("KIDS_AGE", 3);
+define("KIDS_DISCOUNT", 100);
+
+
+
+for ($i = 0 ; $i <= 80 ; $i++){
+    $age = $i;
+    if ($age >= SENIOR_AGE){
+        $price = FARE * ((100 - SENIOR_DISCOUNT) / 100);
+    }else if ($age < KIDS_AGE){
+        $price = FARE * ((100 - KIDS_DISCOUNT) / 100);
+    }else if ($age <= JUIOR_AGE){
+        $price = FARE * ((100 - JUNIOR_DISCOUNT) / 100);
+    }else if ($age > SENIOR_AGE){
+        $price = FARE * ((100 - SENIOR_DISCOUNT) / 100);
+    }else {
+        $price = FARE;
+    }
+    echo 'Iemand van ' . $age . ' betaald ' . $price . ' euro.<br>';
 }
-if ($leeftijd<=12){
-    $bedrag=$bedrag*0.5;
-}
-if ($leeftijd<3){
-    $bedrag=0;
-}
+
+
+
+
 ?>
 </body>
 </html>
