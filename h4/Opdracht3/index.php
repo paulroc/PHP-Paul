ad76ced02b3c54ac2ed67ae214015b848495ef41
Paul <paul.vanbreda@student.rocvf.nl>
2020-06-10 08:36:52 +0200

    h7 af
    Signed-off-by: Paul <paul.vanbreda@student.rocvf.nl>

--- h4/Opdracht3/index.php
+++ h4/Opdracht3/index.php
@@ -13,15 +13,29 @@
 <ul>
 
     <?php
-    $string = array("a","b","c","d");
-    shuffle($string);
-    geefArrayWeerAlslijst($string);
 
-    function   geefArrayWeerAlslijst($arr){
-        foreach ($arr as $arr_element){
-            echo "<li>".$arr_element."</li>";
+
+  echo reverseString("PAUL");
+  
+    function reverseString($originalString){
+        $stringLength = strlen($originalString);
+        $reverseString = " ";
+        for ($i = $stringLength - 1 ; $i >= 0 ; $i--){
+            $reverseString .= substr($originalString, $i, 1);
+
         }
+        return $reverseString;
     }
+
+   // $string = array("a","b","c","d");
+ //   shuffle($string);
+   // geefArrayWeerAlslijst($string);
+
+  //  function   geefArrayWeerAlslijst($arr){
+    //    foreach ($arr as $arr_element){
+    //        echo "<li>".$arr_element."</li>";
+  //      }
+  //  }
     ?>
 
 </ul>
